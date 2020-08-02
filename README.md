00286efeceac6326ad1f9b0ceb195c187ee80b83
Emojigit <55009343+Emojigit@users.noreply.github.com>
2020-08-02 14:25:11 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Pencil: The Minetest Mod
+Pencil is a minetest mod api that can make tools to change the infotext in a block.
+
+![Screenshot](https://raw.githubusercontent.com/Emojigit/minetest-pencil/master/Inkedscreenshot_20200709_185325_LI.jpg)
+
+# API usage
+## pencil.register_pencil(id,name,text,image)
+
+ - id = the name of pencil (e.g. mod:foobar )
+ - name = the name that display in inventory (e.g. FooBar Pencil ). The default is `text.."Pencil"`.
+ - text = The text hat display in the block which right-clicked by the pencil. The default is `Unconfiged`.
+ - image = the image that show in hudbar and inventory (e.g. foobar.png). The default is [`pencil.png`](https://github.com/Emojigit/minetest-pencil/blob/master/pencil/textures/pencil.png)
+
+# Links
+ - [Minetest Forum](https://forum.minetest.net/viewtopic.php?t=25076)
+ - [Minetest ContentDB](https://content.minetest.net/packages/Emojiminetest/pencil)
---
+++ init.lua
@@ -0,0 +1,54 @@
+minetest.log("action","[pencil] Loading")
+pencil = {}
+function pencil.register_pencil(id,name,text,image)
+	local id,name,text,image = id,name,text,image
+	if not(id) then
+		print("[pencil] Cannot register a pencil with no name")
+		return
+	end
+	if not(text) then
+		text = "Unconfiged"
+	end
+	if not(name) then
+		name = text.." Pencil"
+	end
+	if not(image) then
+		image = "pencil.png"
+	end
+	minetest.register_craftitem(id, {
+		description = name,
+		on_use = function(itemstack, user, pointed_thing)
+			local pos = minetest.get_pointed_thing_position(pointed_thing, above)
+			local meta = minetest.get_meta(pos);
+			meta:set_string("infotext",text);
+		end,
+		inventory_image = image
+	})
+	minetest.log("action","[pencil] Register a pencil "..id.." with text "..text)
+end
+
+-- match to old version
+register_pencil = function(id,name,text,image)
+	pencil.register_pencil(id,name,text,image)
+	minetest.log("warning","[pencil] The pencil "..id.." was registered by using \"register_pencil\" , use \"pencil.register_pencil\" instad.")
+end
+
+pencil.register_pencil("pencil:hello_world","Hello World Pencil","Hello World!",nil)
+-- test the warning when using the old function to register pencil
+register_pencil("pencil:blank","Blank Test Pencil",nil,"barrier_inv.png")
+pencil.register_pencil("pencil:eraser","Eraser"," ","eraser.png")
+pencil.register_pencil("pencil:tmp","TMP Pencil"," ",nil)
+pencil.register_pencil("pencil:noaccess","No Access Pencil","Don't Access!",nil)
+
+minetest.register_privilege("add_pencil", {
+	description = "Can use pencil_add command to add pencil."
+})
+minetest.register_chatcommand("pencil_add", {
+	params = "<text>",
+	privs = {add_pencil=true},
+	description = "Change the text on TMP pencil (pencil:tmp)",
+	func = function(name,param)
+		pencil.register_pencil(":pencil:tmp",tostring(param).." TMP Pencil",tostring(param),nil)
+	end,
+})
+minetest.log("action","[pencil] Loaded")
